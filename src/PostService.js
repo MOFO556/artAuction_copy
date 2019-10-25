f4f4b822672f3184bb567e64c1076550c103b657
Andrey <andreandr3evitch@yandex.ru>
2019-10-25 03:22:44 +0300

    Обмен данными между клиентом и сервером #1

---
+++ src/PostService.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+const url = 'http://localhost:5000/api/posts/';
+
+class PostService {
+
+    //GetPosts
+    static getPosts(){
+        return new Promise(async(resolve, reject) =>{
+
+            try {
+
+                const res = await axios.get(url);
+                const data = res.data;
+                resolve(data.map(post =>({
+                    ...post,
+                    createdAt:new Date(post.createdAt)
+                })));
+
+
+            }
+            catch(error)
+            {
+                reject(error);
+            }
+
+        })
+    }
+
+
+    //CreatePorsts
+
+    static insertPost(text){
+        return axios.post(url,{text});
+    }
+    //DeletePosts
+
+    static deletePost(id){
+        return axios.delete(`${url}${id}`);
+    }
+
+}
+
+export default PostService;
