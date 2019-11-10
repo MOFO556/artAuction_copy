f2c78bc9e9f08e0804c0e5817a52ddab7ea434ad
Andrey <andreandr3evitch@yandex.ru>
2019-11-10 12:19:28 +0300

    Верстка экрана загрузки #14

--- src/components/Load00.vue
+++ src/components/Load00.vue
@@ -1,5 +1,10 @@
+
 <template>
-    <h1>Load 0</h1>
+    <div class="outer">
+        <div class="inner">
+            LOAD
+        </div>
+    </div>
 </template>
 
 <script>
@@ -10,5 +15,18 @@
 
 <style scoped>
 
+    .inner{
+        background: grey;
+        width: 360px;
+        height: 640px;
+    }
+
+    .outer
+    {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+    }
 
 </style>
